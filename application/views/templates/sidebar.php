803bb900bd0a29cc80639d8f7d74352104eb5ff3
LUTFHI GANTENG <luthfi.shatara01@gmail.com>
2020-02-07 21:27:10 +0700

    Luthfi V 1.3

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -36,7 +36,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/UI/general.html" class="nav-link">
+                <a href="../identitas/index" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Identitas</p>
                 </a>
