05e4526b4563f79f049fd20a123e483d47ecd533
M.NafisIrfan <nafizbbx@gmail.com>
2020-02-07 22:12:40 +0700

    pai

--- application/views/identitas.php
+++ application/views/identitas.php
@@ -1,17 +1,53 @@
-<div class="content wrapper">
-<div class="content-header">
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0 text-dark">Identitas Santri</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+          </div>       
+        </div>
+      </div>
     </div>
-</div>
+   
+   <section class="content">
+      <table>
+        <tr>
+        <th>No.</th>
+        <th>NIS</th>
+        <th>Nama Lengkap</th>
+        <th>Tempat Lahir</th>
+        <th>Tanggal Lahir</th>
+        <th>Alamat</th>
+        <th>Wali</th>
+        <th>Kelas Pondok</th>
+        <th>Kelas Payung</th>
+        <th>Semester</th>
+        <th>Image</th>
+        <th>Action</th>
+        </tr>
+
+        <?php>
+        $no = 1;
+        foreach ($identitas as $idnt) : ?>
+
+        <tr>
+            <td><?php> echo $no++ ?></td>
+            <td><?php> echo $idnt->nama?></td>
+            <td><?php> echo $idnt->nis?></td>
+            <td><?php> echo $idnt->tempat_lahir?></td>
+            <td><?php> echo $idnt->tanggal_lahir?></td>
+            <td><?php> echo $idnt->alamat?></td>
+            <td><?php> echo $idnt->wali?></td>
+            <td><?php> echo $idnt->kelas_pondok?></td>
+            <td><?php> echo $idnt->kelas_payung?></td>
+            <td><?php> echo $idnt->semester?></td>
+            <td><?php> echo $idnt->image?></td>
+            <td><?php> echo $idnt->action?></td>
+        </tr>
+        <?php> endforeach; ?>
+      </table>
+   </section>
+  </div>
+ 
+
