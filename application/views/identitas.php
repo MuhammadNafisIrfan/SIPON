9b5a49ac9dafeb22c4817ba924676ffad362e844
M.NafisIrfan <nafizbbx@gmail.com>
2020-02-07 23:03:21 +0700

    pai

--- application/views/identitas.php
+++ application/views/identitas.php
@@ -11,9 +11,9 @@
     </div>
    
    <section class="content">
-      <table>
+      <table  class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
         <tr>
-        <th>No.</th>
+        <th>No</th>
         <th>NIS</th>
         <th>Nama Lengkap</th>
         <th>Tempat Lahir</th>
@@ -27,27 +27,25 @@
         <th>Action</th>
         </tr>
 
-        <?php>
+        <?php
         $no = 1;
         foreach ($identitas as $idnt) : ?>
 
         <tr>
-            <td><?php> echo $no++ ?></td>
-            <td><?php> echo $idnt->nama?></td>
-            <td><?php> echo $idnt->nis?></td>
-            <td><?php> echo $idnt->tempat_lahir?></td>
-            <td><?php> echo $idnt->tanggal_lahir?></td>
-            <td><?php> echo $idnt->alamat?></td>
-            <td><?php> echo $idnt->wali?></td>
-            <td><?php> echo $idnt->kelas_pondok?></td>
-            <td><?php> echo $idnt->kelas_payung?></td>
-            <td><?php> echo $idnt->semester?></td>
-            <td><?php> echo $idnt->image?></td>
-            <td><?php> echo $idnt->action?></td>
+            <td><?php echo $no++ ?></td>
+            <td><?php echo $idnt->nama?></td>
+            <td><?php echo $idnt->nis?></td>
+            <td><?php echo $idnt->tempat_lahir?></td>
+            <td><?php echo $idnt->tanggal_lahir?></td>
+            <td><?php echo $idnt->alamat?></td>
+            <td><?php echo $idnt->wali?></td>
+            <td><?php echo $idnt->kelas_pondok?></td>
+            <td><?php echo $idnt->kelas_payung?></td>
+            <td><?php echo $idnt->semester?></td>
+            <td><img src="<?php echo $idnt->image?>" width=90px, height=90px></img></td>
+            <td><?php echo $idnt->action?></td>
         </tr>
-        <?php> endforeach; ?>
+        <?php endforeach; ?>
       </table>
    </section>
   </div>
- 
-
