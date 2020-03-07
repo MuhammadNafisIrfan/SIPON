1c272438a8cb7c8c0393c20fa00ef244093d9e86
LUTFHI GANTENG <luthfi.shatara01@gmail.com>
2020-03-07 20:40:05 +0700

    Luthfi V 5.1

--- application/views/tunggakan.php
+++ application/views/tunggakan.php
@@ -19,15 +19,6 @@
       </div>
     </div>
 
-    <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h3 class="m-0 text-dark">Tunggakan Santri</h3>
-          </div>       
-        </div>
-    
-    </div>
-
 
    <section class="content">
    <button class="btn btn-info btn-sm"><i class="fa fa-plus"data-toggle="modal" data-target="#staticBackdrop"> Tambah DATA</i></button>
