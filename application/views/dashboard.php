8284a3a6438d298d682190b2fc333f1613b1ec86
M.NafisIrfan <nafizbbx@gmail.com>
2020-03-07 14:52:04 +0700

    fddf

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,6 +1,15 @@
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+  <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-light">.</h1>
+          </div>       
+        </div>
+      </div>
+    </div>
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
